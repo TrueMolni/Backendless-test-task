c76471b1f42c8f9fca7cecb0ef36a4bc956cec75
TrueMolni <truemolni@gmail.com>
2023-11-06 18:32:04 +0200

    0.1.0
    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SharedLayout from "./components/SharedLayout";
@@ -16,17 +16,19 @@ const App = () => {
 
   return (
     <BrowserRouter basename="backendless-test-task">
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route
-            index
-            element={React.createElement(
-              lazy(() => import(`./${sortedData[0].path}`))
-            )}
-          />
-          {elements}
-        </Route>
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route
+              index
+              element={React.createElement(
+                lazy(() => import(`./${sortedData[0].path}`))
+              )}
+            />
+            {elements}
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
